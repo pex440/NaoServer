783d52d0215033da56ab733d1efb40b8755936f4
pex440 <giuseppecolucci04@gmail.com>
2022-05-12 00:05:14 +0200

    Aggiunte nuove funzioni alla classe NAO, ora è possibile mandare i comandi come stringhe

--- NAO.py
+++ NAO.py
@@ -7,6 +7,23 @@ class NAO:
         self.session = Session()
         self.session.connect("tcp://"+ip+":"+str(port))
         self.motion = self.session.service("ALMotion")
+    
+    def getMetodiUtilizzabili(self):
+        metodi_utilizzabili = [method_name for method_name in dir(self)
+                  if callable(getattr(self, method_name))]
+        metodi_utilizzabili.remove("__init__")
+        metodi_utilizzabili.remove("eseguiComando")
+        metodi_utilizzabili.remove("getMetodiUtilizzabili")
+
+        return metodi_utilizzabili
+
+    def eseguiComando(self,comando):
+        metodi = self.getMetodiUtilizzabili()
+
+        if comando in metodi:
+            getattr(self,comando)()
+
+
     def camminaAvanti(self,quanto=0.3):
         self.motion.wakeUp()
         self.motion.moveTo(quanto,0,0)
@@ -15,9 +32,20 @@ class NAO:
         self.motion.moveTo(-quanto,0,0)
     def camminaDestra(self,quanto=0.3):
         self.motion.wakeUp()
-        self.motion.moveTo(0,quanto,0)
+        self.motion.moveTo(0,-quanto,0)
     def camminaSinistra(self,quanto=0.3):
         self.motion.wakeUp()
-        self.motion.moveTo(0,-quanto,0)
-    
-#NAO("192.168.1.92")
+        self.motion.moveTo(0,quanto,0)
+
+    def camminaNW(self,quanto=0.3):
+        self.motion.wakeUp()
+        self.motion.moveTo(quanto,quanto,0)
+    def camminaNE(self,quanto=0.3):
+        self.motion.wakeUp()
+        self.motion.moveTo(quanto,-quanto,0)
+    def camminaSW(self,quanto=0.3):
+        self.motion.wakeUp()
+        self.motion.moveTo(-quanto,quanto,0)
+    def camminaSE(self,quanto=0.3):
+        self.motion.wakeUp()
+        self.motion.moveTo(-quanto,-quanto,0)
--- naoserver.py
+++ naoserver.py
@@ -1,28 +1,20 @@
+from distutils.log import debug
 from flask import Flask,render_template,request
 import qi
 from NAO import NAO
+
+
 app = Flask(__name__)
-
 robotIp = "utente.local" # sto usando il robot virtuale di choregraphe
-
+nao = NAO(robotIp)
 @app.route("/")
 def index():
     if request.method == "GET": # todo: passare alle richieste POST
         comando = request.values.get('comando')
-        if comando is not None:
-            if comando == "camminaAvanti":
-                nao = NAO(robotIp)
-                nao.camminaAvanti()
-            elif comando == "camminaIndietro":
-                nao = NAO(robotIp)
-                nao.camminaIndietro()
-            
-               
+        nao.eseguiComando(comando)
+        
+        return render_template("index.html",comandi=nao.getMetodiUtilizzabili())
 
-    return render_template("index.html",comandi=[
-        "camminaAvanti",
-        "camminaIndietro"
-    ])
-
+        
 if __name__ == "__main__":
     app.run(debug=True)
